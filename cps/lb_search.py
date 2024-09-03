5e273e4e62cd73e91b53664503c9b219fcbd7a6c
Blondel MONDESIR <blondel.md@gmail.com>
2024-09-03 11:50:46 -0400

    Use a class structure
    Also prefixing function names with an underscore for good practice

--- cps/lb_search.py
+++ cps/lb_search.py
@@ -1,56 +1,59 @@
 import sqlite3
 from copy import deepcopy
 from itertools import groupby
-from .constants import XKLB_DB_FILE
 
-def query_database(query, params=()):
-    """Executes a query on the database and returns the results."""
-    with sqlite3.connect(XKLB_DB_FILE) as connection:
-        cursor = connection.cursor()
-        cursor.execute(query, params)
-        return [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
+class CaptionSearcher:
+    def __init__(self, db_file):
+        self.db_file = db_file
 
-def merge_captions(captions):
-    """Merges overlapping captions for the same video path."""
-    def get_end(caption):
-        return caption["time"] + (len(caption["text"]) / 4.2 / 220 * 60)
+    def _query_database(self, query, params=()):
+        """Executes a query on the database and returns the results."""
+        with sqlite3.connect(self.db_file) as connection:
+            cursor = connection.cursor()
+            cursor.execute(query, params)
+            return [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
 
-    merged_captions = []
-    for path, group in groupby(captions, key=lambda x: x["path"]):
-        group = list(group)
-        merged_group = deepcopy(group[0])
-        merged_group["end"] = get_end(group[0])
-        for i in range(1, len(group)):
-            if group[i]["time"] <= merged_group["end"]:
-                merged_group["text"] += " " + group[i]["text"]
-                merged_group["end"] = get_end(group[i])
-            else:
-                merged_captions.append(merged_group)
-                merged_group = deepcopy(group[i])
-                merged_group["end"] = get_end(group[i])
-        merged_captions.append(merged_group)
+    def _merge_captions(self, captions):
+        """Merges overlapping captions for the same video path."""
+        def get_end(caption):
+            return caption["time"] + (len(caption["text"]) / 4.2 / 220 * 60)
 
-    return merged_captions
+        merged_captions = []
+        for path, group in groupby(captions, key=lambda x: x["path"]):
+            group = list(group)
+            merged_group = deepcopy(group[0])
+            merged_group["end"] = get_end(group[0])
+            for i in range(1, len(group)):
+                if group[i]["time"] <= merged_group["end"]:
+                    merged_group["text"] += " " + group[i]["text"]
+                    merged_group["end"] = get_end(group[i])
+                else:
+                    merged_captions.append(merged_group)
+                    merged_group = deepcopy(group[i])
+                    merged_group["end"] = get_end(group[i])
+            merged_captions.append(merged_group)
 
-def construct_captions_search_query(term):
-    """Constructs the SQL query to search for captions containing the search term."""
-    query = f"""
-        SELECT media.path, media.title, captions.text, captions.time
-        FROM captions
-        JOIN media ON media.id = captions.media_id
-        WHERE captions.text LIKE ?
-        ORDER BY media.path, captions.time
-    """
-    return query, (f"%{term}%",)
+        return merged_captions
 
-def get_search_terms(term):
-    """Searches for captions matching the term and processes the results."""
-    query, params = construct_captions_search_query(term)
-    captions = query_database(query, params)
-    if not captions:
-        return []
-    
-    merged_captions = merge_captions(captions)
-    titles = list({caption['title'] for caption in merged_captions})
-    
-    return titles
+    def _construct_captions_search_query(self, term):
+        """Constructs the SQL query to search for captions containing the search term."""
+        query = f"""
+            SELECT media.path, media.title, captions.text, captions.time
+            FROM captions
+            JOIN media ON media.id = captions.media_id
+            WHERE captions.text LIKE ?
+            ORDER BY media.path, captions.time
+        """
+        return query, (f"%{term}%",)
+
+    def get_search_terms(self, term):
+        """Searches for captions matching the term and processes the results."""
+        query, params = self._construct_captions_search_query(term)
+        captions = self._query_database(query, params)
+        if not captions:
+            return []
+        
+        merged_captions = self._merge_captions(captions)
+        titles = list({caption['title'] for caption in merged_captions})
+        
+        return titles
