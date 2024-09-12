f96e0bd451a3dd6ef30062be4dce098ef360eb04
Blondel MONDESIR <blondel.md@gmail.com>
2024-09-12 11:26:17 -0400

    Define core tables for xklb db

--- cps/xb.py
+++ cps/xb.py
@@ -1,1 +1,199 @@
+import os
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, create_engine, exc
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from . import logger
 
+log = logger.create()
+
+session = None
+xklb_DB_path = None
+Base = declarative_base()
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    id = Column(Integer, primary_key=True)
+    playlists_id = Column(Integer)
+    size = Column(Integer)
+    duration = Column(Integer)
+    time_created = Column(Integer)
+    time_modified = Column(Integer)
+    time_deleted = Column(Integer)
+    time_downloaded = Column(Integer)
+    fps = Column(Integer)
+    view_count = Column(Integer)
+    path = Column(String)
+    webpath = Column(String)
+    extractor_id = Column(String)
+    title = Column(String)
+    uploader = Column(String)
+    live_status = Column(String)
+    error = Column(String)
+    time_uploaded = Column(Integer)
+    width = Column(Integer)
+    height = Column(Integer)
+    type = Column(String)
+    video_codecs = Column(String)
+    audio_codecs = Column(String)
+    subtitle_codecs = Column(String)
+    video_count = Column(Integer)
+    audio_count = Column(Integer)
+    language = Column(String)
+    subtitle_count = Column(Integer)
+    download_attempts = Column(Integer)
+
+    captions = relationship("Caption", back_populates="media")
+
+    def __repr__(self):
+        return f"<Media(title='{self.title}', path='{self.path}')>"
+
+class Caption(Base):
+    __tablename__ = 'captions'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    time = Column(Integer)
+    text = Column(Text)
+
+    media = relationship("Media", back_populates="captions")
+
+    def __repr__(self):
+        return f"<Caption(media_id={self.media_id}, time={self.time}, text={self.text})>"
+
+class BookMediaMapping(Base):
+    __tablename__ = 'book_media_map'
+
+    book_id = Column(Integer, primary_key=True)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship("Media")
+
+class History(Base):
+    __tablename__ = 'history'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    time_played = Column(Integer)
+    done = Column(Boolean)
+
+    media = relationship("Media", back_populates="history")
+
+class Playlists(Base):
+    __tablename__ = 'playlists'
+
+    id = Column(Integer, primary_key=True)
+    time_modified = Column(Integer)
+    time_deleted = Column(Integer)
+    time_created = Column(Integer)
+    hours_update_delay = Column(Integer)
+    path = Column(String)
+    extractor_key = Column(String)
+    profile = Column(String)
+    extractor_config = Column(String)
+    extractor_playlist_id = Column(String)
+    title = Column(String)
+    uploader = Column(String)
+
+# Define the captions_fts, media_fts, playlists_fts (full-text search tables)
+class CaptionsFTS(Base):
+    __tablename__ = 'captions_fts'
+
+    docid = Column(Integer, primary_key=True)
+    text = Column(Text)
+
+class MediaFTS(Base):
+    __tablename__ = 'media_fts'
+
+    docid = Column(Integer, primary_key=True)
+    title = Column(Text)
+
+class PlaylistsFTS(Base):
+    __tablename__ = 'playlists_fts'
+
+    docid = Column(Integer, primary_key=True)
+    title = Column(Text)
+
+# Define configuration tables for FTS (captions_fts_config, media_fts_config, playlists_fts_config)
+class CaptionsFTSConfig(Base):
+    __tablename__ = 'captions_fts_config'
+
+    id = Column(Integer, primary_key=True)
+    config_value = Column(String)
+
+class MediaFTSConfig(Base):
+    __tablename__ = 'media_fts_config'
+
+    id = Column(Integer, primary_key=True)
+    config_value = Column(String)
+
+class PlaylistsFTSConfig(Base):
+    __tablename__ = 'playlists_fts_config'
+
+    id = Column(Integer, primary_key=True)
+    config_value = Column(String)
+
+# Define data tables for FTS (captions_fts_data, media_fts_data, playlists_fts_data)
+class CaptionsFTSData(Base):
+    __tablename__ = 'captions_fts_data'
+
+    id = Column(Integer, primary_key=True)
+    data = Column(Text)
+
+class MediaFTSData(Base):
+    __tablename__ = 'media_fts_data'
+
+    id = Column(Integer, primary_key=True)
+    data = Column(Text)
+
+class PlaylistsFTSData(Base):
+    __tablename__ = 'playlists_fts_data'
+
+    id = Column(Integer, primary_key=True)
+    data = Column(Text)
+
+# Define document size tables for FTS (captions_fts_docsize, media_fts_docsize, playlists_fts_docsize)
+class CaptionsFTSDocSize(Base):
+    __tablename__ = 'captions_fts_docsize'
+
+    id = Column(Integer, primary_key=True)
+    docsize = Column(Integer)
+
+class MediaFTSDocSize(Base):
+    __tablename__ = 'media_fts_docsize'
+
+    id = Column(Integer, primary_key=True)
+    docsize = Column(Integer)
+
+class PlaylistsFTSDocSize(Base):
+    __tablename__ = 'playlists_fts_docsize'
+
+    id = Column(Integer, primary_key=True)
+    docsize = Column(Integer)
+
+# Define FTS indexes (captions_fts_idx, media_fts_idx, playlists_fts_idx)
+class CaptionsFTSIndex(Base):
+    __tablename__ = 'captions_fts_idx'
+
+    id = Column(Integer, primary_key=True)
+    idx_value = Column(String)
+
+class MediaFTSIndex(Base):
+    __tablename__ = 'media_fts_idx'
+
+    id = Column(Integer, primary_key=True)
+    idx_value = Column(String)
+
+class PlaylistsFTSIndex(Base):
+    __tablename__ = 'playlists_fts_idx'
+
+    id = Column(Integer, primary_key=True)
+    idx_value = Column(String)
+
+# SQLite statistics
+class SQLiteStat1(Base):
+    __tablename__ = 'sqlite_stat1'
+
+    tbl = Column(String, primary_key=True)
+    idx = Column(String)
+    stat = Column(String)
